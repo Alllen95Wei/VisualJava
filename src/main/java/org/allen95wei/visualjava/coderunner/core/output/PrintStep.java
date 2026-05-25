04dd957734a681599a0f6968f6dfa1420c7af53e
Allen <49893966+Alllen95Wei@users.noreply.github.com>
2026-05-25 23:12:09 +0800

    feat(step): 新增 `println` 的 `Step` 實作

---
+++ src/main/java/org/allen95wei/visualjava/coderunner/core/output/PrintStep.java
@@ -0,0 +1,41 @@
+package org.allen95wei.visualjava.coderunner.core.output;
+
+import org.allen95wei.visualjava.coderunner.core.*;
+
+
+public class PrintStep implements Step {
+    private final String result;
+    private final String contextKey;
+
+    public PrintStep(String str) {
+        contextKey = null;
+        result = str;
+    }
+
+    public PrintStep(Store<Object> store) {
+        this(store.get().toString());
+    }
+
+    public PrintStep(String format, Store<?> store) {
+        this(String.format(format, store.get()));
+    }
+
+    public PrintStep(String format, String contextKey) {
+        result = format;
+        this.contextKey = contextKey;
+    }
+
+    @Override
+    public void execute() {
+        System.out.println(result);
+    }
+
+    @Override
+    public void execute(ExecutionContext context) {
+        if (contextKey == null) {
+            throw new RuntimeException("contextKey is null");
+        }
+        Store<?> store = context.getStore(contextKey);
+        System.out.printf(result + "%n", store.get());
+    }
+}
