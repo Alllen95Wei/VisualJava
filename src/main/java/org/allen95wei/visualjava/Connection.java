76b426976a97530375927461af5d8df44fb1361f
STEVEN CHEN <stevenchen76006@gmail.com>
2026-06-05 23:40:20 +0800

    pushing this new theme 4 rd time 6/5 11:40 pm

--- src/main/java/org/allen95wei/visualjava/Connection.java
+++ src/main/java/org/allen95wei/visualjava/Connection.java
@@ -1,6 +1,5 @@
 package org.allen95wei.visualjava;
 
-
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
@@ -16,7 +15,7 @@ public class Connection {
     private Circle toCircle;
 
     // 舊版建構子 / Old constructor
-    // 保留給 Main.java 使用，不要刪掉
+    // 給舊版 Main.java 使用 / Used by old Main.java
     public Connection(
             Block from,
             Block to,
@@ -32,8 +31,24 @@ public class Connection {
         this.toCircle = to.getInputCircle();
     }
 
+    // 朋友版本建構子 / Friend's constructor
+    // 給 Main.java 使用，記錄是哪一個輸出節點拉出的線
+    // Used by Main.java to remember which output node created the line
+    public Connection(
+            Block from,
+            Block to,
+            Circle fromNode,
+            Line line
+    ) {
+        this.from = from;
+        this.to = to;
+        this.fromCircle = fromNode;
+        this.toCircle = to.getInputCircle();
+        this.line = line;
+    }
+
     // 新版建構子 / New constructor
-    // 給多節點積木使用 / Used for blocks with multiple nodes
+    // 給多節點積木使用 / Used for blocks with multiple input/output nodes
     public Connection(
             Block from,
             Block to,
@@ -64,10 +79,22 @@ public class Connection {
         return line;
     }
 
+    // 朋友 Main.java 需要這個 method / Friend's Main.java needs this method
+    public Circle getFromNode() {
+        return fromCircle;
+    }
+
+    // 可選：如果之後需要取得目標節點 / Optional: get target node
+    public Circle getToNode() {
+        return toCircle;
+    }
+
+    // 你的 EditorController.java 用這個 / Your EditorController.java uses this
     public Circle getFromCircle() {
         return fromCircle;
     }
 
+    // 你的 EditorController.java 用這個 / Your EditorController.java uses this
     public Circle getToCircle() {
         return toCircle;
     }
